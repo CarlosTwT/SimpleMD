7a91602eacbc0c79bf3ab9b975d44dc6ce3afd98
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2022-08-23 05:19:38 +0200

    Add files via upload

--- handler.js
+++ handler.js
@@ -175,8 +175,7 @@ module.exports = handler = async (mek, conn, map) => {
 		
        // [ Anonymous ]
        require("./lib/function/anonymous")(msg,conn);
-       }
-
+       
   // [ Afk ]
   if(isGroup){
       await require("./lib/function/afk")(msg,conn);
