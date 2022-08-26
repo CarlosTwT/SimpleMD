342ee61e26f0c75755f3198413598d3aafbad7bb
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2022-08-26 16:00:42 +0200

    Add files via upload

--- command/main/helpcmd.js
+++ command/main/helpcmd.js
@@ -4,30 +4,31 @@ module.exports = {
   name: ["help"].map((v) => v + " <command>"),
   alias: ["help"],
   category: "main",
-  desc: ['Ver información del Comando', '.help <Command>'],
+  desc: ['Ver información de Command', '.help <Command>'],
   async run({msg,conn},{ args, q, map}) {
-      if(!q) return msg.reply("Ejemplo : .help menu",{adReply: true})
+      if(!q) return msg.reply("Example : .help menu",{adReply: true})
 			const name = q.toLowerCase();
 			const { command, prefix } = map;
 			const cmd = command.get(name) || [...command.values()].find((x) => x.alias.find((x) => x == args[0]));
 			if (!cmd || (cmd.category === "private" && !config.owner.includes(msg.sender)))
 				return await msg.reply("Comando no encontrado");
 			helpcmd = global.footer + "\n\n"
-			helpcmd += "*Comando de ayuda*\n\n"
-			helpcmd += monospace(` × Comando : ${q}`) + "\n"
-			helpcmd += monospace(` × Comando de activación : ${cmd.alias.join(", ")}`) + "\n"
+			helpcmd += "*Helper Command*\n\n"
+			helpcmd += monospace(` × Command : ${q}`) + "\n"
+			helpcmd += monospace(` × Triger Command : ${cmd.alias.join(", ")}`) + "\n"
 			helpcmd += monospace(` × Categoría : ${cmd.category}`) + "\n\n"
-			helpcmd += "*Atributo de comando*\n"
+			helpcmd += "*Atributos de comando*\n"
 			helpcmd += monospace(` × isOwner : ${cmd.options.isOwner ? '✅' : '❌'}`) + "\n"
 			helpcmd += monospace(` × isAdmin : ${cmd.options.isAdmin ? '✅' : '❌'}`) + "\n"
 			helpcmd += monospace(` × isBotAdmin : ${cmd.options.isBotAdmin ? '✅' : '❌'}`) + "\n"
 			helpcmd += monospace(` × isGroup : ${cmd.options.isGroup ? '✅' : '❌'}`) + "\n"
-			helpcmd += monospace(` × isPrivate : ${cmd.options.isPrivate ? '✅' : '❌'}`) + "\n\n"
-			helpcmd += "*Comando Descripción*\n"
+                        helpcmd += monospace(` × isPrivate : ${cmd.options.isPrivate ? '✅' : '❌'}`) + "\n"
+			helpcmd += monospace(` × Command Lock : ${map.lockcmd.get(q) ? "✅" : "❌"}`) + "\n\n"
+			helpcmd += "*Command Description*\n"
 			helpcmd += monospace(` × Descripción : ${cmd.desc}`) + "\n"
-			helpcmd += monospace(` × Uso : ${prefix}${cmd.name} ${cmd.use}`) + "\n\n"
-			helpcmd += "*Nota :*\n"
-			helpcmd += ` ➠ *[ ]* = Opcional\n ➠ *|* = O\n ➠ *<>* = Debe ser llenado`
+			helpcmd += monospace(` × *Usos : ${prefix}${cmd.name} ${cmd.use}`) + "\n\n"
+			helpcmd += "*Note :*\n"
+			helpcmd += ` ➠ *[ ]* = Opcional\n *|* = O\n *<>* = Debe completarse`
       msg.reply(helpcmd)
   }
 }
--- command/main/menu.js
+++ command/main/menu.js
@@ -1,13 +1,17 @@
 const { monospace } = require('../../lib/function')
 const fs = require("fs");
 let multi_pref = new RegExp("^[" + "!#%&?/;:,.~-+=".replace(/[|\\{}()[\]^$+*?.\-\^]/g, "\\$&") + "]");
+const moment = require("moment");
+const processTime = (timestamp, now) => {
+	return moment.duration(now - moment(timestamp * 1000)).asSeconds();
+};
 
 module.exports = {
   name: ['menu'].map((v) => v + ''),
   alias: ["cmd","menu"],
   category: "main",
   desc: "Mostrar comandos",
-  async run({msg,conn}, {map}) {
+  async run({conn, msg},{map, q}){
     let { body , reply} = msg
     let pref = multi_pref.test(body) ? body.split("").shift() : ".";
     let locale = "id"
@@ -26,35 +30,68 @@ module.exports = {
 	  const { prefix, command } = map;
 		const cmds = command.keys();
 		let category = [];
-    
-    for (let cmd of cmds) {
-				let info = command.get(cmd);
-				if (!cmd) continue;
-				if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
-				cteg = info.category || "No Category";
-				if (info.type == "changelog") continue;
-				if (!cteg || cteg === "private") cteg = "owner";
-				if (Object.keys(category).includes(cteg)) category[cteg].push(info);
-				else {
-					category[cteg] = [];
-					category[cteg].push(info);
-				}
-			}
-			
+    try {
+      if(q){
+        for(const cmd of cmds){
+          let info = command.get(cmd);
+          if (!cmd) continue;
+		  		if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
+		  		cteg = info.category || "No Category";
+		  		if (info.type == "changelog") continue;
+			  	if (!cteg || cteg === "private") cteg = "owner";
+		  		if (Object.keys(category).includes(cteg)) category[cteg].push(info);
+		  		else {
+		  		  category[cteg] = [];
+				  	category[cteg].push(info);
+		  		}
+        }
+        teks = global.footer + " *[ Beta✓ ]*\n\n"
+		  	teks += monospace(" ❏ Library : Baileys-MD") + "\n"
+		  	teks += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
+		  	teks += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n\n"
+		  	teks += monospace(`Hola, @${sender.split("@")[0]} Aquí mi Comando`) +`\n\n`;
+		  	teks += `*乂 ${q.toUpperCase()}*\n`
+		  	nganu = category[q]
+		  	if(nganu == undefined) throw "Categoría no encontrada!!"
+        for(let i of nganu){
+          teks += monospace(` × ${pref + i.name} ${map.lockcmd.get(i.name) ? "❌" : ""}`) + "\n"
+        }
+        teks += "\n*Bot aún en etapa de desarrollo*"
+        msg.reply(teks,{withTag: true})
+      } else {
+        for (let cmd of cmds){
+          let info = command.get(cmd);
+			  	if (!cmd) continue;
+		  		if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
+		  		cteg = info.category || "No Category";
+		  		if (info.type == "changelog") continue;
+		  		if (!cteg || cteg === "private") cteg = "owner";
+			  	if (Object.keys(category).includes(cteg)) category[cteg].push(info);
+		  		else {
+		  		  category[cteg] = [];
+			  		category[cteg].push(info);
+			  	}
+        }
 			menu = global.footer + " *[ Beta✓ ]*\n\n"
-			menu += monospace(" ❏ Librería : Baileys-MD") + "\n"
-			menu += monospace(" ❏ Autor : " + "@" + config.owner[0].split("@")[0] )+ "\n"
-			menu += monospace(" ❏ Prefijo : [ " + pref + " ]") + "\n"
-			menu += monospace(" ❏ Fecha : " + date) + "\n"
-			menu += monospace(" ❏ Tiempo : " + time) + "\n\n"
-			menu += monospace(`Hola, @${sender.split("@")[0]} Aquí mi lista de comandos`) +`\n\n`;
+			menu += monospace(" ❏ Library : Baileys-MD") + "\n"
+			menu += monospace(" ❏ Author : " + "@" + config.owner[0].split("@")[0] )+ "\n"
+			menu += monospace(" ❏ Prefix : [ " + pref + " ]") + "\n"
+			menu += monospace(" ❏ Date : " + date) + "\n"
+			menu += monospace(" ❏ Time : " + time) + "\n"
+		  menu += monospace(" ❏ Speed :  " + processTime(msg.messageTimestamp, moment()) + " Seccond") + "\n\n"
+		  menu += "*Este script de bot es : https://github.com/CarlosTwT*\n_La marca significa que el propietario está deshabilitando un error o una característica!!_\n\n"
+			menu += monospace(`Hoka, @${sender.split("@")[0]} Aquí mi lista de comandos`) +`\n\n`;
 			const keys = Object.keys(category)
+			menu += "*乂 MENÚ DE CATEGORÍA*\n"
+			for(let o of keys){
+			  menu += monospace(` × ${pref + msg.command} ${o}`) + "\n"
+			}
+			menu += "\n"
 			for(let key of keys){
 			  menu += `*乂 ${key.toUpperCase()}*\n`
-			  menu += `${category[key].map((cmd) => monospace(` × ${cmd.options.noPrefix ? "" : pref}${cmd.name}`)).join("\n")}` + "\n\n"
+			  menu += `${category[key].map((cmd) => monospace(` × ${cmd.options.noPrefix ? "" : pref}${cmd.name} ${map.lockcmd.get(cmd.name) ? "❌" : ""}`)).join("\n")} ` + "\n\n"
 			}
-			menu += `_Nota: Usa ${prefix}help <comando> para ver información de comando_`
-			
+			menu += `_Nota: Ekvia ${prefix}help <comando> para ver información de comando_`
 			
 			const buttons = [
            { buttonId: `.owner`,buttonText:{displayText: 'Owner'}, type : 1},
@@ -69,18 +106,9 @@ module.exports = {
            withTag: true
          }
        conn.sendMessage(msg.from, buttonMessage, {quoted : msg})
-
-/*const { generateWAMessageFromContent } = require ("@adiwajshing/baileys")
-
-prep = generateWAMessageFromContent(msg.from, { liveLocationMessage: { 
-degreesLatitude: 0, degreesLongitude: 0,
-caption: menu,
-sequenceNumber: 0, timeOffset: 0, jpegThumbnail: null
-}}, { quoted: msg
-					})
-
-return conn.relayMessage(msg.from, prep.message, { messageId: prep.key.id })*/
-
-
+      }
+    } catch (e){
+      global.error(msg.command, e, msg)
+    }
   }
 }
