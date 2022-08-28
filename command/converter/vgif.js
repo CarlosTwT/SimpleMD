0df13e9c7ba5ba2419d66c677c1a30fe83de34ab
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2022-08-27 21:34:24 -0500

    Create vgif.js

---
+++ command/converter/vgif.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "vgif",
+	category: "converter",
+	desc: "Cambia el video a gif con audio",
+    isQuoted: true,
+  isMedia: {
+    isQVideo: true,
+  },
+	async run({ msg,conn }) {
+        let buff = await  msg.quoted.download()
+        await conn.sendMessage(
+            msg.from,
+            {
+                video: buff,
+                gifPlayback: true,
+            },
+            { quoted: msg}
+        )
+	}
+};
