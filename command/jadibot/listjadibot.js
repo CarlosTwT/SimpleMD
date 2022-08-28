26efee8f98830df6ca1c570e9d9be6615e20819a
I'am Carlos <103902036+CarlosTwT@users.noreply.github.com>
2022-08-28 04:41:46 +0200

    Mini mejoras😨

--- command/jadibot/listjadibot.js
+++ command/jadibot/listjadibot.js
@@ -1,3 +1,5 @@
+const prettyms = require('pretty-ms')
+
 module.exports = {
   name: "listjadibot",
   alias: ["listjadibot","listbot"],
@@ -5,12 +7,14 @@ module.exports = {
   desc: "mostrando a los usuarios que se suben a los bots!!",
   async run({conn, msg}){
     try {
-      let user = [... new Set([...global.conns.filter(conn => conn.user).map(conn => conn.user)])]
-      te = "*Lista de Jadibot*\n\n"
+      let user = [... new Set([...global.conns.filter(conn => conn).map(conn => conn)])]
+      te = "*Lista Jadibot*\n\n"
+      te += ` Total : ${global.conns.length}\n\n`
       for(let i of user){
-        y = await conn.decodeJid(i.id)
-        te += " × Usuario : @" + y.split("@")[0] + "\n"
-        te += " × Nombre : " + i.name + "\n\n"
+        y = await conn.decodeJid(i.user.id)
+        te += " × User : @" + y.split("@")[0] + "\n"
+        te += " × Nombre : " + i.user.name + "\n"
+        te += " × Tiempo : " + await prettyms(Date.now() - i.time, { verbose: true, }) + "\n\n"
       }
       user != "" ? await msg.reply(te,{withTag : true}) : await msg.reply("_*No hay usuarios que sean sub-bots..*_")
     } catch (e){
